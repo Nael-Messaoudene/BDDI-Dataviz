a218a549777574ee9f6e7c01ab379834880e7566
naelmessaoudene <naelmessaoudene@gmail.com>
2019-12-02 23:37:24 +0100

    wip animation bars

--- src/scripts/athlete-number.js
+++ src/scripts/athlete-number.js
@@ -1,3 +1,6 @@
+import * as d3 from "d3";
+import {gsap} from "gsap";
+
 class athleteNumber {
 
     constructor(){
@@ -5,8 +8,97 @@ class athleteNumber {
     }
 
     init(){
-        console.log('ooooo');
+        this.chart()
     }
+
+    chart(){
+
+        let svgWidth = 500;
+        let svgHeight = 300;
+
+        const radius =  10;
+
+        const margin =  10;
+        let svg = d3.select('svg')
+            .attr("width", svgWidth + 2  * margin)
+            .attr("height", svgHeight )
+            .attr("class", "bar-chart");
+
+
+        let dataset = [
+            {
+                sport: 'Athlètes olympiques',
+                value: 250.9,
+                color: '#ff455e'
+            },
+            {
+                sport: 'Athlètes paralympiques',
+                value: 75.1,
+                color: '#64dfc7'
+            }
+            ];
+
+        let barPadding = 15;
+        let barWidth = (svgWidth / dataset.length);
+
+
+
+        const barGroups = svg.selectAll()
+            .data(dataset)
+            .enter()
+            .append('g')
+
+
+        barGroups
+            .append("rect")
+
+            .attr("rx", radius)
+            .attr("ry", radius)
+            .attr('y',  (g) => svgHeight - (g.value))
+            .attr('height', (g) => (g.value))
+            .attr("width", barWidth - barPadding)
+            .style("fill",  (g) => (g.color))
+            .attr("transform", function (d, i) {
+                let translate = [barWidth * i, 0];
+                return "translate("+ translate +")";
+            });
+
+        barGroups.append('text')
+            .attr('class', 'sport-info')
+            .attr('x', svgWidth / 2 + margin)
+            .attr('y', 40)
+            .attr('text-anchor', 'middle')
+            .text((g) => (g.sport))
+
+        const allG = d3.selectAll('g');
+
+        allG
+            .attr("class", "box");
+
+        const allRect = d3.selectAll('.box rect');
+        allRect
+            .attr('class', 'rect-item');
+
+
+        console.log(allRect);
+
+
+
+        let test =document.querySelectorAll('.rect-item');
+
+        console.log(test);
+
+        test.forEach( (e,i)=>{
+            console.log(dataset[i].value);
+            gsap.fromTo('.rect-item',{ height: 0}, {duration:1, height: dataset[i].value ,stagger:{each: 0.9}});
+        });
+
+
+
+
+
+    }
+
 }
 
 export default athleteNumber;
