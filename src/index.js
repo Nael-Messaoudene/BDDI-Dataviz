57eb7b9ab61e2390ef758c1df22b719b46e6c09a
naelmessaoudene <naelmessaoudene@gmail.com>
2019-12-02 13:40:02 +0100

    init athlete number files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./style/styles.scss";
 
 console.log("index!");
+import athlete from './scripts/athlete-number'
+
+const App = {
+
+    init(){
+        window.addEventListener('DOMContentLoaded', () => {
+            new athlete();
+        });
+    }
+};
+
+App.init();
---
+++ src/scripts/athlete-number.js
@@ -0,0 +1,12 @@
+class athleteNumber {
+
+    constructor(){
+        this.init();
+    }
+
+    init(){
+        console.log('ooooo');
+    }
+}
+
+export default athleteNumber;
