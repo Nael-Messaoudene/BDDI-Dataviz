5a57db964583b58336b01047a331c8e287622358
Sowa Killian <killian.sowa@m6.fr>
2019-12-02 13:10:49 +0100

    morphing animation added on loading screen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import LoadingScreen from "./scripts/loading-screen";
 
 //Component LoadingScreen
 const loadingscreen = ((loadingscreen) => {
+    loadingscreen.iconsMorphing();
     loadingscreen.animateScreens();
 })(LoadingScreen);
 
