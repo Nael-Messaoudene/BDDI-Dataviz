b93539c459bf678c477f0fefb99a2b3306f0c6ac
naelmessaoudene <naelmessaoudene@gmail.com>
2019-12-03 23:48:23 +0100

    add scrollmagic + design v2

--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -1,4 +1,10 @@
-import { gsap } from 'gsap';
+var gsap = require('gsap');
+// TweenMax/TimelineMax will be global in v2. In v3, they will be on the gsap object
+var TweenMax = TweenMax || gsap.TweenMax;
+var TimelineMax = TimelineMax || gsap.TimelineMax;
+factory(require('scrollmagic'), TweenMax, TimelineMax);
+import ScrollMagic from 'scrollmagic';
+ScrollMagic.registerGSAP(gsap);
 
 class conversation{
     constructor(){
@@ -18,12 +24,23 @@ class conversation{
     conversation(){
         console.log('conversation');
 
-        gsap.to('.message--box', {
-            duration: 2,
-            opacity: 1,
-            delay: 1, stagger:{
-                each: 0.9
-            } });
+        var controller = new ScrollMagic.Controller();
+
+
+
+        var revealElements = document.getElementsByClassName("message--box");
+
+        let tween = gsap.fromTo(revealElements,{opacity:0, y:20,},{opacity:1, duration:1, y:0});
+        for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+            new ScrollMagic.Scene({
+                triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+                offset: 50,												 // start a little later
+                triggerHook: 0.9,
+            })
+                .setTween(tween)
+                .setClassToggle(revealElements[i], "visible") // add class toggle
+                .addTo(controller);
+        }
 
     }
     // bindEvents(){
