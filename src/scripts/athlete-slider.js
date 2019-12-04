eb63053d0f39cbb372ff88b75c6a953687fe5cd2
Sowa Killian <killian.sowa@m6.fr>
2019-12-04 11:36:16 +0100

    athlete item profile is dynamic

--- src/scripts/athlete-slider.js
+++ src/scripts/athlete-slider.js
@@ -39,14 +39,74 @@ const AthleteSlider = {
                         '                    <img src="'+'skiing.c962b042'+'.svg'+'"></div>' +
                         '                <div class="athlete-slider-slides-item__name">' + athlete.name +
                         '                </div>' +
-                        '            </div>');
+                        '                <div class="athlete-slider-slides-item__sport">' + athlete.sport +
+                        '                </div>' +
+                        '                <div class="athlete-slider-slides-item__birthdate">' + athlete.birthdate +
+                        '                </div>' +
+                        '                <div class="athlete-slider-slides-item__handicap">' + athlete.handicap +
+                        '                </div>' +
+                        '                <div class="athlete-slider-slides-item__history">' + athlete.history +
+                        '                </div>' +
+                        '       </div>');
                 });
 
                 const athletesList = document.querySelectorAll(".athlete-slider-slides-item");
+                console.log(athletesList);
 
-                athletesList.forEach(athleteItem => {
+                athletesList.forEach((athleteItem) => {
+                    const athleteInfos = {
+                        athleteName: athleteItem.querySelector(".athlete-slider-slides-item__name").innerText,
+                        athleteSport: athleteItem.querySelector(".athlete-slider-slides-item__sport").innerText,
+                        athleteBirthdate: athleteItem.querySelector(".athlete-slider-slides-item__birthdate").innerText,
+                        athleteHandicap: athleteItem.querySelector(".athlete-slider-slides-item__handicap").innerText,
+                        athleteHistory: athleteItem.querySelector(".athlete-slider-slides-item__history").innerText,
+                    };
+
+                    //console.log(document.querySelector(".athlete-slider-slides-item").innerHTML);
+
                     athleteItem.addEventListener("click", () => {
                         this.athleteItem.classList.add('athlete-item-visible');
+                        this.athleteItem.insertAdjacentHTML('beforeend', '        <span class="athlete-item-close"></span>\n' +
+                            '        <span class="athlete-item-backgroundname">\n' +
+                                         athleteInfos.athleteName +
+                            '            DAVET\n' +
+                            '        </span>\n' +
+                            '        <div class="athlete-item-profile" style="background-image: url(\'/assets/images/daviet.jpg\')">\n' +
+                            '            <div class="athlete-item-profile__icon">\n' +
+                            '                <img src="/assets/images/skiing.svg">\n' +
+                            '            </div>\n' +
+                            '        </div>\n' +
+                            '        <div class="athlete-item-description">\n' +
+                            '            <div class="athlete-item-description-sport">\n' +
+                                        athleteInfos.athleteSport +
+                            '            </div>\n' +
+                            '            <div class="athlete-item-description-name">\n' +
+                                        athleteInfos.athleteName +
+                            '            </div>\n' +
+                            '            <div class="athlete-item-description-datas">\n' +
+                            '                <div class="athlete-item-description-medals">\n' +
+                            '                    <p class="athlete-item-description-title">MEDAILLES</p>\n' +
+                            '                </div>\n' +
+                            '                <div class="athlete-item-description-origin">\n' +
+                            '                    <p class="athlete-item-description-title">ORIGINAIRE DE</p>\n' +
+                            '                </div>\n' +
+                            '            </div>\n' +
+                            '            <div class="athlete-item-description-period">\n' +
+                            '                <p class="athlete-item-description-title">DATE DE NAISSANCE</p>\n' +
+                            '                <p class="athlete-item-description-text">' + athleteInfos.athleteBirthdate + ' </p>\n' +
+                            '            </div>\n' +
+                            '            <div class="athlete-item-description-birthdate">\n' +
+                            '\n' +
+                            '            </div>\n' +
+                            '            <div class="athlete-item-description-handicap">\n' +
+                            '                <p class="athlete-item-description-title">HANDICAP</p>\n' +
+                            '                <p class="athlete-item-description-text">' + athleteInfos.athleteHandicap + '</p>\n' +
+                            '            </div>\n' +
+                            '            <div class="athlete-item-description-history">\n' +
+                            '                <p class="athlete-item-description-title">PALMARÈS</p>\n' +
+                            '                <p class="athlete-item-description-text">'+ athleteInfos.athleteHistory +'</p>\n' +
+                            '            </div>\n' +
+                            '        </div>');
                     });
                 });
 
