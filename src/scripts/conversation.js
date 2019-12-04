70c8fa3ee0b4685f95520556c6d5205991d68832
naelmessaoudene <naelmessaoudene@gmail.com>
2019-12-04 23:32:00 +0100

    add final conversation

--- src/scripts/conversation.js
+++ src/scripts/conversation.js
@@ -29,8 +29,33 @@ class conversation{
             viewFactor: 0.4
         });
 
+        ScrollReveal().reveal('.message--box', {
+            container: '.final-desktop',
+            delay: 375,
+            duration: 500,
+            reset: true,
+            distance: '-20px',
+            viewOffset: {
+                top: 0,
+            },
+            viewFactor: 0.4
+        });
+
         window.addEventListener('resize', function () {
 
+            ScrollReveal().reveal('.message--box', {
+                container: '.final-mobile',
+                delay: 100,
+                duration: 500,
+                reset: true,
+                distance: '-20px',
+                viewOffset: {
+                    top: 0,
+                },
+                viewFactor: 0.4
+            });
+
+            //mobile
             ScrollReveal().reveal('.message--box', {
                 container: '.wrap-message',
                 delay: 100,
@@ -55,6 +80,18 @@ class conversation{
             viewFactor: 0.4
         });
 
+        //mobile final
+        ScrollReveal().reveal('.message--box', {
+            container: '.final-mobile',
+            delay: 100,
+            duration: 500,
+            reset: true,
+            distance: '-20px',
+            viewOffset: {
+                top: 0,
+            },
+            viewFactor: 0.4
+        });
 
     }
 
