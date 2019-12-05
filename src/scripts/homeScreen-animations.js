df6e30f9bf8aa1737bbabf185702accb722881f8
naelmessaoudene <naelmessaoudene@gmail.com>
2019-12-05 21:09:52 +0100

    add blob animation

--- src/scripts/homeScreen-animations.js
+++ src/scripts/homeScreen-animations.js
@@ -16,19 +16,18 @@ class homeScreenAnimation {
 
         window.addEventListener('mousemove',function (e) {
             let home = document.querySelector('.homescreen');
-           let  w = home.clientWidth;
+            let  w = home.clientWidth;
             let h = home.clientHeight;
             let offsetX = 0.5 - e.pageX / w;
             let offsetY = 0.5 - e.pageY / h;
 
             let div = document.querySelectorAll("div");
-            console.log(div.toString());
 
             for(let i = 0; i < div.length; i++) {
                 let data = div[i].getAttribute("data-offset");
                 data = parseInt(data);
-                let trans = "translate3d("+Math.round(offsetX*data)+"%,"+Math.round(offsetY*data)+"%, 0)";
-                div[i].style.transform = trans;
+                let transform = "translate3d("+Math.round(offsetX*data)+"%,"+Math.round(offsetY*data)+"%, 0)";
+                div[i].style.transform = transform;
             }
         })
     }
