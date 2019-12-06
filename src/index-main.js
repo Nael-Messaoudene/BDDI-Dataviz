9eae5cf23a5807cbb9c7b02a7b8ecb458a0fbff1
Lou Batier <batierlou@gmail.com>
2019-12-06 11:26:20 +0100

    fixing js building issue on multipage

---
+++ src/index-main.js
@@ -0,0 +1,47 @@
+import "./style/styles.scss";
+
+import LoadingScreen from "./scripts/loading-screen";
+import Popin from "./scripts/popin";
+import athlete from './scripts/athlete-number'
+import AthleteSlider from "./scripts/athlete-slider";
+
+import Hearings from "./scripts/hearings";
+import Worldmap from "./scripts/worldmap";
+import conversation from './scripts/conversation'
+import homeScreenAnimation from './scripts/homeScreen-animations'
+
+
+//Component LoadingScreen
+const loadingscreen = ((loadingscreen) => {
+    loadingscreen.goToTop();
+    loadingscreen.iconsMorphing();
+    loadingscreen.animateScreens();
+})(LoadingScreen);
+
+//Component Worldmap
+const worldmap = ((worldmap) => {
+    worldmap.initMap();
+})(Worldmap);
+
+//Component AthleteSlider
+const athleteslider = ((athleteslider) => {
+    athleteslider.getAthletesDatas();
+    athleteslider.displayAthletes();
+    athleteslider.slideAthletes();
+})(AthleteSlider);
+
+const hearings = ((hearings) => {
+    hearings.drawGraph();
+})(Hearings);
+
+
+const App = {
+
+    init(){
+        window.addEventListener('DOMContentLoaded', () => {
+            new conversation();
+            new athlete();
+        });
+    }
+};
+App.init();
